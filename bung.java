26d14dfc0b0b10c8d87cfb2aac75c3d8425028ff
AhmedMohamed21 <82438623+AhmedMohamed21@users.noreply.github.com>
2021-06-11 13:27:40 +0300

    begin

--- bung.java
+++ bung.java
@@ -1,1 +1,11 @@
-
+import javax.swing.*;
+public class bung extends JFrame{
+	bung(){
+		super("wellcome");
+		setSize(500,500);
+		setVisible(true);
+	}
+	public static void main(String arg[]) {
+		new bung();
+	}
+}
---
+++ work/src/work/Bung.java
@@ -0,0 +1,14 @@
+package work;
+
+import javax.swing.*;
+
+public class Bung extends JFrame {
+Bung(){
+	super("wellcome");
+	setSize(500, 500);
+	setVisible(true);
+}
+public static void main(String arg[]) {
+	new Bung();
+}
+}
